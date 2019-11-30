26a22802593297f630a93ad958849cabb8be9cb7
Shubham Naik <GS-1497@GSLAB.COM>
2019-11-30 21:11:11 +0530

    flaky & pytst

--- tests/smoke_tests/test_login.py
+++ tests/smoke_tests/test_login.py
@@ -5,10 +5,8 @@ from web.Pages.home_page import HomePage
 
 
 class TestLogin(object):
-
-    def __init__(self):
-        self.login_page = LoginPage()
-        self.home_page = HomePage()
+    login_page = LoginPage()
+    home_page = HomePage()
 
     def test_user_login_with_valid_credentials(self):
         self.home_page.navigate_to_input_box()
@@ -20,7 +18,8 @@ class TestLogin(object):
         # assert (home_res == True)
 
 
-if __name__ == "__main__":
-    test_login = TestLogin()
-    test_login = test_login.test_user_login_with_valid_credentials()
+# if __name__ == "__main__":
+#     test_login = TestLogin()
+#     test_login = test_login.test_user_login_with_valid_credentials()
+
 
